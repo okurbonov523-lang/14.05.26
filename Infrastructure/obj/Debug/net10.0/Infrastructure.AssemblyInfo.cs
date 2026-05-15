5dc56285875b026263f10bba15e36419ed68d5a8
murodov yahyo <murodovyahyo72@gmail.com>
2026-05-15 16:02:55 +0500

    asd

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c7e4a78f7990e3ce60678ba0f7ee974ef078bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044d5ce26d28d319cf4b439673399033dce1e65f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
